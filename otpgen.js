723f0f4e63a60c5397951ffafdc53b3f5dc7182b
assim fayas <asim1fayas@gmail.com>
2023-08-12 11:32:42 +0530

    js otp generator

---
+++ otpgen.js
@@ -0,0 +1,7 @@
+//using math.random
+console.log((Math.random().toString().substr(-4)));
+//using date.now()
+console.log(Date.now().toString().substr(-4));
+
+let otp=Math.random().toString().substr(-4)
+console.log(otp,"otp");
